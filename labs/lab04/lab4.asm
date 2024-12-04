b246419f711ee63ed3940eef88bb21ff4b9df94b
Alisa Bakhtiyarova <alissssey@gmail.com>
2024-12-04 22:59:26 +0300

    feat(main): make course structure

---
+++ labs/lab04/lab4.asm
@@ -0,0 +1,16 @@
+; hello.asm
+SECTION .data ; Начало секции данных
+	lab4: DB 'Alisa Bakhtiyarova', 10
+	
+	lab4Len: EQU $-lab4 ; Длина строки
+SECTION .text ; Начало секции кода
+GLOBAL _start
+_start: ; Точка входа в программу
+mov eax,4 ; Системный вызов для записи (sys_write)
+mov ebx,1 ; Описатель файла '1' - стандартный вывод
+mov ecx,lab4 ; Адрес строки lab4 в ecx
+mov edx,lab4Len ; Размер строки lab4
+int 80h ; Вызов ядра
+mov eax,1 ; Системный вызов для выхода (sys_exit)
+mov ebx,0 ; Выход с кодом возврата '0' (без ошибок)
+int 80h ; Вызов ядра
